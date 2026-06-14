ea9b294506e347ac744e5091ea896fdf33b2f30e
Serhii <serhii.tymoshchuk@icloud.com>
2026-06-14 04:30:52 +0200

    complete hw tasks

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { authenicate } from '../middleware/authenticate.js';
+import { authenticate } from '../middleware/authenticate.js';
 import { updateUserAvatar } from '../controllers/userController.js';
 import { upload } from '../middleware/multer.js';
 
@@ -7,7 +7,7 @@ const userRouter = Router();
 
 userRouter.patch(
   'users/me/avatar',
-  authenicate,
+  authenticate,
   upload.single('avatar'),
   updateUserAvatar,
 );
